73d58bf5a5d012354e72a440331896460d0411b2
shmdt2020 <shmdt2020@outlook.jp>
2020-05-14 22:19:32 +0900

    learn LazyJsonTest.cs

--- OpenTweenLinux.Tests/src/LazyJsonTest.cs
+++ OpenTweenLinux.Tests/src/LazyJsonTest.cs
@@ -19,16 +19,14 @@
 // the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 // Boston, MA 02110-1301, USA.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.Serialization;
-using System.Text;
 using System.Threading.Tasks;
-using OpenTween.Api;
+using System.Text;
+using System.IO;
+using System.Net.Http;
 using Xunit;
+using System;
+using System.Runtime.Serialization;
+//using OpenTween.Api;
 
 namespace OpenTween.Connection
 {
@@ -37,18 +35,15 @@ namespace OpenTween.Connection
         [Fact]
         public async Task LoadJsonAsync_Test()
         {
-            var body = Encoding.UTF8.GetBytes("\"hogehoge\"");
+            byte[] body = Encoding.UTF8.GetBytes("\"hogehoge\"");
             using var bodyStream = new MemoryStream(body);
-            using var response = new HttpResponseMessage();
-            response.Content = new StreamContent(bodyStream);
-
+            using var response = new HttpResponseMessage() { Content = new StreamContent(bodyStream) };
             using var lazyJson = new LazyJson<string>(response);
 
             // この時点ではまだレスポンスボディは読まれない
             Assert.Equal(0, bodyStream.Position);
 
-            var result = await lazyJson.LoadJsonAsync()
-                .ConfigureAwait(false);
+            string result = await lazyJson.LoadJsonAsync().ConfigureAwait(false);
 
             Assert.Equal("hogehoge", result);
         }
@@ -56,11 +51,9 @@ namespace OpenTween.Connection
         [Fact]
         public async Task LoadJsonAsync_InvalidJsonTest()
         {
-            var body = Encoding.UTF8.GetBytes("### Invalid JSON ###");
+            byte[] body = Encoding.UTF8.GetBytes("### Invalid JSON ###");
             using var bodyStream = new MemoryStream(body);
-            using var response = new HttpResponseMessage();
-            response.Content = new StreamContent(bodyStream);
-
+            using var response = new HttpResponseMessage() { Content = new StreamContent(bodyStream) };
             using var lazyJson = new LazyJson<string>(response);
 
             // この時点ではまだレスポンスボディは読まれない
@@ -75,19 +68,14 @@ namespace OpenTween.Connection
         [Fact]
         public async Task IgnoreResponse_Test()
         {
-            using var bodyStream = new InvalidStream();
-            using var response = new HttpResponseMessage();
-
             // IgnoreResponse() によってレスポンスの Stream が読まれずに破棄されることをテストするため、
-            // 読み込みが行われると IOException が発生する InvalidStream クラスを bodyStream に使用している
-            response.Content = new StreamContent(bodyStream);
-
+            // 読み込みが行われると例外が発生する下記の InvalidStream クラスを bodyStream に使用している
+            using var bodyStream = new InvalidStream();
+            using var response = new HttpResponseMessage() { Content = new StreamContent(bodyStream) };
             using var lazyJson = new LazyJson<string>(response);
 
             // レスポンスボディを読まずに破棄
-            await Task.FromResult(lazyJson)
-                .IgnoreResponse()
-                .ConfigureAwait(false);
+            await Task.FromResult(lazyJson).IgnoreResponse().ConfigureAwait(false);
 
             Assert.True(bodyStream.IsDisposed);
         }
